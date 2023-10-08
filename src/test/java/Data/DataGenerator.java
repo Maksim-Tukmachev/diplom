474fa38a649453970086b433d5e45ad600fad772
MaksTukmachev <verdo.ya.ry19@gmail.com>
2023-10-08 21:05:04 +0400

    edit sqlHelp

--- src/test/java/Data/DataGenerator.java
+++ src/test/java/Data/DataGenerator.java
@@ -7,37 +7,15 @@ import java.util.Random;
 
 public class DataGenerator {
 
-    public static String name(){
+    public static String name() {
         Faker faker = new Faker();
         return faker.name().fullName();
     }
 
-    public static String currentYear(){
+    public static String currentYear() {
         LocalDate currentDate = LocalDate.now();
         int year = currentDate.getYear() - 2000;
         return Integer.toString(year);
-    }
-
-    public static String validFutureYear(){
-        Random random = new Random();
-        int i = random.nextInt(5) + 1;
-
-        LocalDate futureDate = LocalDate.now().plusYears(i);
-        int year = futureDate.getYear() - 2000;
-
-        return Integer.toString(year);
-    }
-
-    public static String pastYear() {
-        Random random = new Random();
-        int i = random.nextInt(18) + 1;
-        return String.format("%02d", i);
-    }
-
-    public static String currentMonth() {
-        LocalDate currentDate = LocalDate.now();
-        int month = currentDate.getMonthValue();
-        return String.format("%02d", month);
     }
 
     public static String validFutureYear() {
@@ -47,7 +25,7 @@ public class DataGenerator {
         LocalDate futureDate = LocalDate.now().plusYears(i);
         int year = futureDate.getYear() - 2000;
 
-        return Integer.toString(year)
+        return Integer.toString(year);
     }
 
     public static String pastYear() {
